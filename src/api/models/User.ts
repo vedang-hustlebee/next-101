1a5a9911594ce9881ac2397cf1d187afcec48abc
vedang <vedang@hustlebee.net>
2024-06-05 13:02:04 +0530

    ui and test setup

---
+++ src/api/models/User.ts
@@ -0,0 +1,36 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../config/config';
+
+class User extends Model {
+  public id!: number;
+  public username!: string;
+  public email!: string;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+  },
+  {
+    sequelize,
+    modelName: 'User',
+  }
+);
+
+export default User;
---
+++ src/app/users/page.tsx
@@ -0,0 +1,7 @@
+export default function users() {
+    return (
+        <div className="bg-white text-black">
+            <p>this is list of users</p>
+        </div>
+    )
+}
